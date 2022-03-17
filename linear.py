031492d74721271086d99962470cc2c030154b24
Bryon Wilkins <bryonwilkins94@gmail.com>
2022-03-17 14:48:33 -0400

    complete the course and update the readme.

--- linear.py
+++ linear.py
@@ -1,4 +1,6 @@
 from fractions import Fraction
+from math import isclose
+from sympy import radsimp, sqrt
 
 def scale(scalar, row):
   return list(map(lambda x: x * scalar, row))
@@ -20,6 +22,12 @@ def pivotEntry(rowNum, matrix):
 def newEmptyMatrix(matrix):
   return list(map(lambda _: [], matrix))
 
+def swapRow(i, j, matrix):
+  tempRow = matrix[i]
+  matrix[i] = matrix[j]
+  matrix[j] = tempRow
+  return matrix
+
 def rref(matrix):
   newMatrix = matrix
   for i in range(0, height(matrix)):
@@ -28,7 +36,13 @@ def rref(matrix):
     tempMatrix = newEmptyMatrix(matrix)
     pivotRowNum = i
     if(pivotEntry(i, newMatrix) == 0):
-      return newMatrix
+      swapped = False
+      for j in range(i+1, height(newMatrix)):
+        if newMatrix[j][i] != 0:
+          newMatrix = swapRow(i, i+1, newMatrix)
+          swapped = True
+      if not swapped:
+        continue
     pivotRow = scale(1/pivotEntry(i, newMatrix), newMatrix[pivotRowNum])
     tempMatrix[i] = pivotRow
     for j in range(0, height(matrix)):
@@ -43,7 +57,11 @@ def printMatrix(matrix):
   for row in matrix:
     r = ""
     for entry in row:
-      r+= str(Fraction(entry).limit_denominator()) + "\t"
+      try:
+        v = str(radsimp(entry)) +"\t"
+      except:
+        v = str(int(entry) if entry % 1 == 0 else Fraction(entry)) +"\t"
+      r+= v
     print(r)
 
 def augmentMatrix(originalMatrix, augment):
@@ -110,6 +128,9 @@ def specialFlip2By2(matrix):
     [-matrix[1][0], matrix[0][0]],
   ]
 
+def determinateOf3By3(matrix):
+  return ruleOfSarrus(matrix)
+
 def ruleOfSarrus(matrix):
   augMatrix = list(map(lambda row: row[0:2], matrix))
   newMatrix = augmentMatrix(matrix, augMatrix)
@@ -138,7 +159,7 @@ def determinateOf4By4(matrix):
       if(i == j):
         continue
       matrixToSarrus.append(matrix[j][1:])
-    valsToSum.append(sign*matrix[i][0]*ruleOfSarrus(matrixToSarrus))
+    valsToSum.append(sign*matrix[i][0]*determinateOf3By3(matrix))
   return sum(valsToSum)
 
 def transpose(matrix):
@@ -149,6 +170,9 @@ def transpose(matrix):
       newRow.append(row[i])
     newMatrix.append(newRow)
   return newMatrix
+
+def extractColumnMatrix(index, matrix):
+  return transpose([columnAt(index, matrix)])
     
 def addMatrix(m1, m2):
   newMatrix = []
@@ -163,6 +187,12 @@ def projectionMatrix(matrix):
       inverseOfMatrix(dotProd(transpose(matrix), matrix))
     ), 
     transpose(matrix)
+  )
+
+def projectWithOrthonormal(orthonormalMatrix, x):
+  return dotProd(
+    dotProd(orthonormalMatrix, transpose(orthonormalMatrix)),
+     x
   )
 
 def leastSquares(matrix, b):
@@ -200,44 +230,43 @@ def transformVectorInBasis(transMatrix, basisMatrix, vectorMatrix):
     transMatrixInBasis(transMatrix, basisMatrix),
     vectorMatrix
   )
-print("~~1~~")
-transMatrix = [ #A
-  [-2,-2,1],
-  [1,0,-2],
-  [0,1,0]
-]
-basisMatrix = [ #C
-  [1,0,2],
-  [-1,1,1],
-  [1,-1,-2]
-]
-vector = transpose([[5,4,-2]])
-printMatrix(transformVectorInBasis(transMatrix, basisMatrix, vector))
 
-print("~~2~~")
-transMatrix = [ #A
-  [-2,-5],
-  [3,1],
-]
-basisMatrix = [ #C
-  [2,-6],
-  [1,-2],
-]
-vector = transpose([[2,-2]])
-printMatrix(transformVectorInBasis(
-  transMatrix, 
-  basisMatrix, 
-  transformToNewBasis(basisMatrix, vector)
-))
+def isOrthogonal(v1, v2):
+  return dotProd([v1],transpose([v2]))[0][0] == 0
 
-print("~~3~~")
-transMatrix = [ #A
-  [-1, 3],
-  [2,1],
-]
-basisMatrix = [ #C
-  [-4,4],
-  [4,0]
-]
-vector = transpose([[2,3]])
-printMatrix(transformVectorInBasis(transMatrix, basisMatrix, vector))
+def isNormalized(vector):
+  return isclose(dotProd([vector], transpose([vector]))[0][0], 1)
+
+def isOrthonormal(v1, v2):
+  return isNormalized(v1) and isNormalized(v2) and isOrthogonal(v1,v2)
+
+def lengthOfVector(vectorMatrix):
+  valsToSum = []
+  for entryRow in vectorMatrix:
+    valsToSum.append(entryRow[0]**2)
+  return sqrt(sum(valsToSum))
+
+def asUnitVector(vectorMatrix):
+  return scaleMatrix(1/lengthOfVector(vectorMatrix), vectorMatrix)
+
+def asOrthogonalVectorTo(vectorMatrix, orthonormalMatrix):
+  valToSum =[]
+  for i in range(0, width(orthonormalMatrix)):
+    orthogonalVector = extractColumnMatrix(i, orthonormalMatrix)
+    valToSum.append(scaleMatrix(dotProd(
+      transpose(vectorMatrix), orthogonalVector)[0][0], 
+      orthogonalVector
+    ))
+  summedMatrix = valToSum[0]
+  for i in range(1, len(valToSum)):
+    summedMatrix = addMatrix(summedMatrix, valToSum[i])
+  return addMatrix(vectorMatrix, scaleMatrix(-1, summedMatrix)) 
+
+def convertToOrthonormalBasis(matrix):
+  matrixVOrtho = asUnitVector(extractColumnMatrix(0, matrix))
+  for c in range(1, width(matrix)):
+    columnVector = extractColumnMatrix(c, matrix)
+    vectorAsOrthogonal = asOrthogonalVectorTo(columnVector, matrixVOrtho)
+    u = asUnitVector(vectorAsOrthogonal)
+    matrixVOrtho = augmentMatrix(matrixVOrtho, u)
+  return matrixVOrtho
